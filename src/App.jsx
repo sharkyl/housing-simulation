282a11aaeb2212b1dcffe9de6c1a63553962f3e0
Sharky Laguana <sharky@bandago.com>
2024-12-04 14:16:40 -0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HousingSimulation from './components/HousingSimulation'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background p-8">
+      <HousingSimulation />
+    </div>
+  )
+}
+
+export default App
