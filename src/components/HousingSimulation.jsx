f28cfe1c249b77d96553f6416ccd549b968015f5
Sharky Laguana <sharky@bandago.com>
2025-01-11 08:07:10 -0800

    Update HousingSimulation.jsx

--- src/components/HousingSimulation.jsx
+++ src/components/HousingSimulation.jsx
@@ -128,9 +128,11 @@ const calculateAvailability = () => {
   return (
     <div className="max-w-7xl mx-auto p-6 bg-white rounded-lg shadow-lg">
       
-      <h1 className="text-2xl font-bold mb-6">SF Homeless Oversight Commission - Data Officer Report, December 2024</h1>
-Before using this simulation model, please watch the YouTube video of this report here: <br></br>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/yachetMaSwE?si=tbtIUSTAjGVI5CIl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <h1 className="text-2xl font-bold mb-6">San Francisco Homeless Oversight Commission Data Officer Report</h1>
+Before using the simulation model, please watch the YouTube video of this report here: <br></br>
+<div class="video-container">
+<iframe src="https://www.youtube.com/embed/yachetMaSwE?si=tbtIUSTAjGVI5CIl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
 <br></br><br></br>
             <h2>Permanent Supportive Housing Simulation</h2>
       When you are done watching the video, you can play with the permanent supportive housing simulation below. This model simulates the impact of changing permanent supportive housing inventory supply, the number of people flowing into housing, and the average length of stay of people in housing.<br></br>
